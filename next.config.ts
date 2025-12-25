8541aa0a0b6e0f18993c6ef6d6886bbac98133c5
shijas <shijas@example.com>
2025-12-25 16:09:44 +0530

    Fix: Externalize Coinbase SDK and Solana dependencies

--- next.config.ts
+++ next.config.ts
@@ -17,8 +17,26 @@ const nextConfig: NextConfig = {
       },
     ],
   },
-  webpack: (config) => {
-    config.externals.push('pino-pretty', 'lokijs', 'encoding')
+  webpack: (config, { isServer }) => {
+    // Externalize problematic dependencies
+    config.externals.push(
+      'pino-pretty',
+      'lokijs',
+      'encoding',
+      'axios',
+      '@solana/kit',
+      '@solana/web3.js',
+      '@coinbase/cdp-sdk',
+      '@base-org/account'
+    )
+
+    // Ignore optional dependencies
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@coinbase/cdp-sdk': false,
+      '@base-org/account': false,
+    }
+
     return config
   },
 };
